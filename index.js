a604c3a85fa0e177431eaf85c10496058c8dab31
varsha <varshavemula2905@gmail.com>
2026-09-18 10:09:58 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let express=require('express');
+let mongoose=require('mongoose');
 let hrroutes=require('./routes/hr_routes');
+let emproutes=require('./routes/emp_routes');
 let app=express();
 
+mongoose.connect("mongodb://localhost:27017/hrmanagement").then(
+    ()=>console.log("Database connected successfully")
+).catch((err)=>console.log(err));
+app.use(express.json());
 app.use("/api/hr", hrroutes);
+//open browser localhost:3000/api/hr/employees
+app.use("/api/emp", emproutes);
+//open browser localhost:3000/api/emp/viewtasks
 
 //run the server in port 3000
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 });
+
--- routes/emp_routes.js
+++ routes/emp_routes.js
@@ -1,12 +1,26 @@
 let express=require('express');
 let router=express.Router();
 
-router.get("/hr", (req,res)=>{
-    res.send("Hr called")
+router.post("/register", (req,res)=>{
+    let data=req.body;
+    res.send(data.nameco);
 });
 
-router.post("/assign-task",(req,res)=>{
-    res.send("hr assigned the work")
+router.post("/login",(req,res)=>{
+    res.send("login page called")
 });
+
+router.get("/viewtasks",(req,res)=>{
+    res.send("viewtask page called called")
+});
+
+router.get("/viewtodos",(req,res)=>{
+    res.send("viewtodo's page called")
+});
+
+router.put("/updateprofile",(req,res)=>{
+    res.send("update profile page called")
+});
+
 
 module.exports=router;
