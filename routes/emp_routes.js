a3385b2d36c61358bbeb537043f9fab32b9b737d
varsha <varshavemula2905@gmail.com>
2026-09-23 10:43:25 +0530

    third commit {CRUD}

--- routes/emp_routes.js
+++ routes/emp_routes.js
@@ -1,14 +1,30 @@
 let express=require('express');
 let router=express.Router();
+let bcrypt=require('bcrypt');
+let {users}=require('../models/users');
 
-router.post("/register", (req,res)=>{
+router.post("/register",async (req,res)=>{
     let data=req.body;
-    res.send(data.nameco);
-});
+    data.password=await bcrypt.hash(data.password,10);
+    let newuser=new users(data);
+    let result=await newuser.save();
+    res.send(result);
+})
 
-router.post("/login",(req,res)=>{
-    res.send("login page called")
-});
+router.post("/login",async (req,res)=>{
+    let user=await users.findOne({email:req.body.email});
+    if(user){
+        let passmatch=await bcrypt.compare(req.body.password,user.password);
+        if(passmatch){
+            res.send("Login success");
+        }else{
+            res.send("password invalid");
+        }
+
+    }else{
+        res.send("Email Invalid");
+    }
+})
 
 router.get("/viewtasks",(req,res)=>{
     res.send("viewtask page called called")
--- routes/hr_routes.js
+++ routes/hr_routes.js
@@ -1,10 +1,14 @@
 let express=require('express');
 let router=express.Router();
+let {users}=require('../models/users');
 
-router.get("/employees", (req,res)=>{
-    res.send("Employees called")
+router.get("/employees",async (req,res)=>{
+    let result=await users.find();
+    result.password=undefined;
+    res.send(result);
 });
-
+//open postman => choose get method
+//localhost:3000/api/hr/employees
 router.post("/assign-task",(req,res)=>{
     res.send("assign task page called")
 });
